0fc7c309b6e2e41ae4944c151649fd9ef9b5e713
Thom Root <thomaslroot@gmail.com>
2024-05-28 20:42:54 -0400

    Day 7 complete. Total time: 70m

--- 07.rb
+++ 07.rb
@@ -1,15 +1,13 @@
 #!/usr/bin/ruby
 
-require 'pry'
-
 class Hand
 
 	attr_accessor :bid, :type, :value
 
 	def initialize cards, bid
 		@cards, @bid = cards, bid.to_i
-		cardCounts = @cards.chars.uniq.to_h{|c|[c,@cards.count(c)]}
-		@type = cardCounts.values.sort.join.to_i
+		@cardCounts = @cards.chars.uniq.to_h{|c|[c,@cards.count(c)]}
+		@type = @cardCounts.values.sort.join.to_i
 		@value = @cards.gsub('A','E').gsub('K','D')
 			.gsub('Q','C').gsub('J','B').gsub('T','A').to_i(16)
 	end
@@ -18,6 +16,20 @@ class Hand
 		typeComparison = other.type <=> self.type
 		return typeComparison unless typeComparison == 0
 		self.value <=> other.value
+	end
+
+	def wildJ!
+		return self unless @cards.include? 'J'
+		if @cards == 'JJJJJ'
+			@type, @value = 5, 0x11111
+			return self
+		end
+		typeDigits = @cardCounts.reject{|d|d=='J'}.values.sort
+		typeDigits[-1] += @cardCounts['J']
+		@type = typeDigits.join.to_i
+		@value = @cards.gsub('A','E').gsub('K','D')
+			.gsub('Q','C').gsub('J','1').gsub('T','A').to_i(16)
+		self
 	end
 end
 
@@ -28,4 +40,6 @@ hands = lines.map{|line| Hand.new(*line.split)}.sort
 winnings = 1.upto(hands.count).map{|rank| rank * hands[rank-1].bid}
 puts winnings.sum
 
-binding.pry
+hands = hands.map(&:wildJ!).sort
+winnings = 1.upto(hands.count).map{|rank| rank * hands[rank-1].bid}
+puts winnings.sum
