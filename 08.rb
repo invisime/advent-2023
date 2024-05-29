69da51e37be2028d2b04136dfd02fabb64f516fb
Thom Root <thomaslroot@gmail.com>
2024-05-28 22:57:17 -0400

    Day 8 complete. Total time: 50m

--- 08.rb
+++ 08.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/ruby
 
-require 'pry'
-
 # lines = IO.readlines('sample08.txt').map(&:strip)
 lines = IO.readlines('input08.txt').map(&:strip)
 
@@ -19,3 +17,21 @@ until node == 'ZZZ'
 	step += 1
 end
 puts step
+
+unfinishedNodes = nodes.keys.select{|name| name.end_with?('A')}
+stepsRequired = 1
+step = 0
+while unfinishedNodes.any?
+	nextNodes = []
+	unfinishedNodes.each do |name|
+		nextNodeName = nodes[name][steps[step % steps.count]]
+		if nextNodeName.end_with? 'Z'
+			stepsRequired = stepsRequired.lcm(step + 1)
+		else
+			nextNodes.push nextNodeName
+		end
+	end
+	unfinishedNodes = nextNodes
+	step += 1
+end
+puts stepsRequired
