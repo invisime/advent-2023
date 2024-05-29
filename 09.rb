615de62a211b5814fc4c284c7facf76bbafff761
Thom Root <thomaslroot@gmail.com>
2024-05-29 08:07:34 -0400

    Day 9 complete. Time: 2hrs.

--- 09.rb
+++ 09.rb
@@ -4,22 +4,24 @@ class History
 	def initialize line
 		@values = line.split.map(&:to_i)
 		@derivatives = [@values]
-		until @derivatives[-1].all?(&:zero?)
-			addPartialDerivative
-		end
+		addNextDerivative until @derivatives.last.all?(&:zero?)
 	end
 
-	def addPartialDerivative
-		1.upto(@derivatives.count) do |depth|
-			@derivatives[depth] ||= []
-			diffRow = @derivatives[depth-1]
-			diff = diffRow[-1-@derivatives[depth].count] - diffRow[-2-@derivatives[depth].count]
-			@derivatives[depth].unshift diff
-		end
+	def addNextDerivative
+		diffRow = @derivatives.last
+		nextRow = (1...diffRow.count).map {|i| diffRow[i] - diffRow[i-1]}
+		@derivatives.push nextRow
 	end
 
 	def extrapolatedNextValue
 		@derivatives.map(&:last).sum
+	end
+
+	def extrapolatedPreviousValue
+		values = @derivatives.map(&:first).reverse
+		value = values[0]
+		values[1..-1].each {|v| value = v - value}
+		value
 	end
 end
 
@@ -28,3 +30,5 @@ lines = IO.readlines('input09.txt').map(&:strip)
 
 histories = lines.map{|line| History.new line}
 puts histories.map(&:extrapolatedNextValue).sum
+
+puts histories.map(&:extrapolatedPreviousValue).sum
